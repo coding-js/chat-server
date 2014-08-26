ad1b4b3ccf1691b50e825072b9eb20ada9a467ac
James Blashill <james@blashill.com>
2014-08-26 09:03:39 -0400

    step2
    single chat room

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var socketio = require('socket.io');
+var sockets
 
 var httpServer = http.createServer(function(req, res){
   res.writeHead(404, "NOT FOUND");
@@ -8,9 +9,30 @@ var httpServer = http.createServer(function(req, res){
 
 var socketIOServer = socketio(httpServer);
 
+var broadcastMessage = function(message) {
+  console.log("Broadcasting message to " + socketIOServer.sockets.sockets.length + " clients");
+  socketIOServer.sockets.emit('message', message);
+};
+
 socketIOServer.on('connection', function(socket) {
   socket.on('subscribe', function(name) {
-    socket.emit('message', 'hello ' + name + "!");    
+    socket.name = name;
+    console.log('Got subscription from ' + name);
+    broadcastMessage({
+      from: "Server",
+      text: name + " joined the chat!"
+    });    
+  });
+  socket.on('message', function(message) {
+    console.log("Received message: " + JSON.stringify(message));
+    broadcastMessage(message);
+  });
+  socket.on('disconnect', function() {
+    console.log(socket.name + " disconnected");
+    broadcastMessage({
+      from: "Server",
+      text: socket.name + " left the chat!"
+    });
   });
 });
 
